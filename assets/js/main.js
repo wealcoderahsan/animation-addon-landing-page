882338327c4c8963bef91ea34d5cd79c34451c24
ahsan <ahsan.wealcoder@gmail.com>
2025-01-30 16:13:15 +0600

    hero section

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// const toggleBtn = document.querySelector("#theme-toggle");
+
+// document.addEventListener("DOMContentLoaded", () => {
+
+//     // toggleBtn.addEventListener("click", () => {
+//     //     document.body.classList.toggle("light-mode");
+//     //   });
+
+// });
